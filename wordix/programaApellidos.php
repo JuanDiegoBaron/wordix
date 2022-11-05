4be09318d2b794d2d5a0e4d803896117100db3e5
Nico13A <nicoantinao1998@gmail.com>
2022-11-05 10:09:48 -0300

    cambios function cargarColeccionPalabras

--- wordix/programaApellidos.php
+++ wordix/programaApellidos.php
@@ -24,8 +24,8 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* ... COMPLETAR ... Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "AZUL", "PERA", "PERROS", "GUITARRA", "TECHO"
     ];
 
     return ($coleccionPalabras);
