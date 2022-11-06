9b42f50b81f00e6ead01a29b38b25f91ac2d9c6c
Nico13A <nicoantinao1998@gmail.com>
2022-11-06 13:39:38 -0300

    funcion seleccionarOpcion

--- wordix/programaApellidos.php
+++ wordix/programaApellidos.php
@@ -8,7 +8,7 @@ include_once("wordix.php");
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
 /* ... COMPLETAR ... */
 /*Baron Juan Diego - 3284 - TUDW - juandiegobaron19@gmail.com - JuanDiegoBaron*/
-/*Antinao, Nicolas - 4353 - TUDW - nicoantinao1998@gmail.com - Nico13A*/
+/*Antinao Nicolas - 4353 - TUDW - nicoantinao1998@gmail.com - Nico13A*/
 
 
 
@@ -34,7 +34,12 @@ function cargarColeccionPalabras()
 
 /* ... COMPLETAR ... */
 
+/**
+ * Devuelve el nombre del jugador ingresado por el usuario en minuscula si solo ingreso letras.
+ * @return string
+ */
 function solicitarJugador(){
+    //String $nombre
     echo "Ingrese el nombre de un jugador: ";
     $nombre = trim(fgets(STDIN));
     $nombre = strtolower($nombre);
@@ -43,6 +48,44 @@ function solicitarJugador(){
         $nombre = strtolower(trim(fgets(STDIN)));
     }
     return $nombre;
+}
+
+/**
+ * 
+ */
+function seleccionarOpcion() {
+    echo "Ingrese un numero de opcion: ";
+    $numOpcion = trim(fgets(STDIN));
+    switch ($numOpcion) {
+        case 1:
+            echo "Jugar al Wordix con una palabra elegida \n";
+            break;
+        case 2:
+            echo "Jugar al Wordix con una palabra aleatoria \n";
+            break;
+        case 3:
+            echo "Mostrar una partida \n";
+            break;
+        case 4:
+            echo "Mostrar la primer partida ganadora \n";
+            break;
+        case 5:
+            echo "Mostrar resumen de Jugador \n";
+            break;
+        case 6:
+            echo "Mostrar listado de partidas ordenadas por jugador y por palabra \n";
+            break;
+        case 7:
+            echo "Agregar una palabra de 5 letras a Wordix \n";
+            break;
+        case 8:
+            echo "Salir \n";
+            break;
+        default:
+            echo "Opcion invalida";
+            break;
+    }
+    return $numOpcion . "\n";
 }
 
 
